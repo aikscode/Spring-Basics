546f5fdc9e76648e66d314a1d669afc270d1465d
Aikins <aikinsakenten@gmail.com>
2024-02-17 21:11:20 +0000

    blueprint of spring context basics

---
+++ src/main/java/org/example/Parrot.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Parrot {
+    public String parrotName;
+
+//    public Parrot(String parrotName){
+//        this.parrotName = parrotName;
+//    }
+
+    public String getParrotName() {
+        return this.parrotName;
+    }
+
+    public void setParrotName(String parrotName) {
+        this.parrotName = parrotName;
+    }
+}
